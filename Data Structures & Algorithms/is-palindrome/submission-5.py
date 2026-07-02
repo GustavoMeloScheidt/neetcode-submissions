f50d30a7dccd6e679b85a779f554a4911b80524a
Gustavo Scheidt <59480755+GustavoMeloScheidt@users.noreply.github.com>
2026-07-02 14:52:02 +0200

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # the logic for this solution is that we will go through the string
+        # if it is an alphanumeric character, we will (after lowercasing it)
+        # add it to the new string, and then it is the same as when reversed, it passes
+        string = ''
+        for c in s:
+            if c.isalnum(): # if it constains alphanumeric characters 
+                string += c.lower()
+        return string == string[::-1] # compare the string with its reversed
