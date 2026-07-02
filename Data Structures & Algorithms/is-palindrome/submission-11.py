2d3b006d8229bc8bd5209ba069813086f4f43142
Gustavo Scheidt <59480755+GustavoMeloScheidt@users.noreply.github.com>
2026-07-02 16:51:29 +0200

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Now, for this pointers solution, the idea is that we will have two pointers:
+        # One at the beginning of the string and another at the end of the string
+        # And for each step, we compare them to see if they are the same
+        # Of course, we need to remove spaces and ponctuations, for that we have 
+        # two ways to do it: either a .isalnum() function, or we build the alphaNumerical 
+        # function, basing ourselves in the ASCII values
+        l = 0 # Left Pointer
+        r = len(s) - 1 # Right Pointer
+        while l < r:
+
+            # # if they have not met each other yet and it is not an AlphaNum, we just increment (ignore it and continue)
+            while l < r and not self.AlphaNumerical(s[l]):
+                l += 1
+            while r > l and not self.AlphaNumerical(s[r]):
+                r -= 1
+
+            # Compare them, if they are different, return False already
+            if s[l].lower() != s[r].lower(): 
+                return False
+            l = l + 1
+            r = r - 1
+        # if it passes the entire while and does not goes to False, it must be true
+        return True
+
+    # Creating our own AlphaNumerical function instead of using .isalnum():
+    def AlphaNumerical(self, c):
+        # In python we can get the ASCII value of a character by using the ord function
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
