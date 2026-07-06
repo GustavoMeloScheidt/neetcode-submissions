7a488383a9d4980444cee09a9844e46e32be5560
Gustavo Scheidt <59480755+GustavoMeloScheidt@users.noreply.github.com>
2026-07-06 18:40:46 +0200

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Using the logic that we can count how many characters are in 
+        # each string and then we can combine the characters with the 
+        # same amount together (by adding them into a dictionary)
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26 
+            for c in s:
+                count[ord(c) - ord("a")] += 1 # here we did the substraction to go from values 0(a) to 25(z)
+            # since our count is a lit, but lists cannot be keys, we change it to a tuple
+            # res[count].append(s)
+            res[tuple(count)].append(s)
+            # the count would give something like this: (1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0) -> eat
+
+        return list(res.values())
